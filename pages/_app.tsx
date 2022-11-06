dea074508754623ae4c472f34cd437f29051b9ac
CheolWoong Choi <bluesky4381@naver.com>
2022-11-06 15:47:01 +0900

    프로젝트 셋팅 - eslint & prettier 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import "@/public/normalize.css";
-import { GlobalStyle } from "@/styles/GlobalStyle";
 
 import type { AppProps } from "next/app";
+
+import { GlobalStyle } from "@/styles/GlobalStyle";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
