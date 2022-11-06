89fe07176ef2a125e77d8fbabd25e900ad49d303
CheolWoong Choi <bluesky4381@naver.com>
2022-11-06 15:56:59 +0900

    프로젝트 셋팅 - axios & react-query 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,29 @@
-import "@/public/normalize.css";
+import '@/public/normalize.css';
 
-import type { AppProps } from "next/app";
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
-import { GlobalStyle } from "@/styles/GlobalStyle";
+import { GlobalStyle } from '@/styles/GlobalStyle';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+  const cache = createCache({ key: 'next' });
+
   return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+    <CacheProvider value={cache}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+    </CacheProvider>
   );
 }
